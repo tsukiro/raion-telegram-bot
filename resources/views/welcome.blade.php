7d1d3747b43672809166f594f642c99bb8b46812
Bastian Vergara <bastian@rasen.cl>
2021-05-18 20:55:18 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Raion Soft
                 </div>
 
                 <div class="links">
